6d6d559c6c73819cdb9b01a71e752e1c78397529
Ahmad Mahmoud <ahmadmahmoudmain@gmail.com>
2024-11-01 14:46:18 +0200

    Add validation methods

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double income, loanAmount;
-        int creditScore, loanType, repaymentPeriod;
+        double income;
+        int creditScore, repaymentPeriod, loanAmount;
         boolean existingDebt, employment;
+        String loanType;
 
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the desired loan amount: ");
-        loanAmount = input.nextDouble();
+        loanAmount = input.nextInt();
 
         System.out.print("Enter your credit score: ");
-        creditScore = input.nextInt();
+        creditScore = scoreVali(input.nextInt());
 
-        System.out.print("Do you have any existing loan? ");
-        existingDebt = input.nextBoolean();
+        System.out.print("Do you have any existing loan? (1 - Yes | 2 - No) ");
+        existingDebt = existingVali(input.nextInt());
 
         System.out.print("Enter the loan type (1 - Mortgage | 2 - Personal): ");
-        loanType = input.nextInt();
+        loanType = typeVali(input.nextInt());
 
-        System.out.print("Are you employed? ");
-        employment = input.nextBoolean();
+        System.out.print("Are you employed? (1 - Yes | 2 - No) ");
+        employment = empVali(input.nextInt());
 
         System.out.print("Enter your monthly income: ");
         income = input.nextInt();
@@ -30,5 +31,21 @@ public class Main {
         repaymentPeriod = input.nextInt();
 
         input.close();
+    }
+
+    public static int scoreVali(int score){
+        return something;
+    }
+
+    public static boolean existingVali(int input){
+        return something;
+    }
+
+    public static String typeVali(int input){
+        return something;
+    }
+
+    public static boolean empVali(int input){
+        return something;
     }
 }
